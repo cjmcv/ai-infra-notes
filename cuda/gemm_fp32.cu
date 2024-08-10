d89ed26387d33e6299271298bdd50f475ddca417
cjmcv <398342899@qq.com>
2024-08-10 15:36:30 +0800

    [cuda] Add split-k.

--- cuda/gemm_fp32.cu
+++ cuda/gemm_fp32.cu
@@ -86,7 +86,9 @@ void CleanUpEnvironment() {
 void GenMatrix(const int height, const int width, float *mat) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            mat[i*width + j] = (float)rand() / RAND_MAX + (float)rand() / (RAND_MAX*RAND_MAX);
+            // mat[i*width + j] = ((float)rand() / RAND_MAX) * 200 - 100; // float: -100 ~ 100
+            mat[i*width + j] = (float)(rand() % 200 - 100); // int: -100 ~ 100
+            // printf("%f, ", mat[i*width + j]);
         }
     }
 }
@@ -197,8 +199,8 @@ __global__ void GemmKernelv2(const int M, const int N, const int K,
                              const float *B, const int ldb,
                              float *C, const int ldc) {
 
-    int i = blockIdx.y * blockDim.y + threadIdx.y;
-    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    int gid_y = blockIdx.y * blockDim.y + threadIdx.y;
+    int gid_x = blockIdx.x * blockDim.x + threadIdx.x;
 
     __shared__ float a_shared[BLOCK_SIZE][BLOCK_SIZE];
     __shared__ float b_shared[BLOCK_SIZE][BLOCK_SIZE];
@@ -206,8 +208,8 @@ __global__ void GemmKernelv2(const int M, const int N, const int K,
     float c_sub_acc = 0;
     // 按 K 方向分块读入共享内存，一次读一个block
     for (int bk = 0; bk < K; bk += BLOCK_SIZE) {
-        a_shared[threadIdx.y][threadIdx.x] = A[i * lda + (bk + threadIdx.x)];
-        b_shared[threadIdx.y][threadIdx.x] = B[(bk + threadIdx.y) * ldb + j];
+        a_shared[threadIdx.y][threadIdx.x] = A[gid_y * lda + (bk + threadIdx.x)];
+        b_shared[threadIdx.y][threadIdx.x] = B[(bk + threadIdx.y) * ldb + gid_x];
         // 等待块内线程同步
         __syncthreads();
 
@@ -219,15 +221,154 @@ __global__ void GemmKernelv2(const int M, const int N, const int K,
         __syncthreads();
     }
 
-    C[i * ldc + j] += c_sub_acc;
+    C[gid_y * ldc + gid_x] += c_sub_acc;
 }
 
 // CUDA version 3.
-//   分析v2，计算的过程实质为全局内存->共享内存->寄存器内存，则v2的k循环中需重复访问的数据存在于共享内存中。
-// 就会有重复的从共享内存读取数据到寄存器。可考虑子在一次读取到共享内存后，再进分块一次读取到寄存器中，
-// 使重复读取数据进行计算的操作放到更快的寄存器中完成。
+//   基于v2的split-k实现，主要针对m和n很小，但k很大的情况。这种情况常见于conv的im2col得到的矩阵中（height/width小channel大的conv）。 
+// 假设m和n均为64，而k为3200，此时观察v3会发现，只有一个block的线程在执行，而block里每个线程的计算任务量随k的增大而增大，
+// 则会造成使用的线程数少，硬件资源无法充分调用，且每个线程使用周期长的问题。
+//   具体做法：
+//   把gemm的k方向split成多个k_n更小的k_size，从而得到了k_n个 [m, k_tile] x [k_tile, n]矩阵乘，
+// 每个矩阵乘的k loop大小缩短，从而每个线程的计算时间缩短，并且可以创建更多的线程数量来执行计算。
+//   为简化方案，令split-k为2，并基于v2修改（出于简化思路考虑，与其他版本的优化方式不冲突）：
+// 线程数x方向多一倍，按M*2N的大小分配，即原线程(i,j)对应C矩阵的(i,j)元素的计算，
+// 现在是(i,j)[0~M,0~N],(i, j-N)[0~M,N~2N]两个线程同时对(i,j)元素负责。
+// 而(i,j)负责k从0到N/2的部分，(i, j-N)负责从N/2到N的部分。最后两个线程reduce到一起输出给C(i,j)
+//// CPU 分析步骤（按k方向两段拆分）：
+// 1）将内层循环拆分
+// for (i = 0; i < M; ++i) {
+//     for (j = 0; j < N; ++j) {
+//         c_sub_acc1 = 0, c_sub_acc2 = 0;
+//         for (k = 0; k < K/2; k++) {
+//             c_sub_acc1 += A[i][k] * B[k][j];
+//         }
+//         for (k = K/2; k < K; k++) {
+//             c_sub_acc2 += A[i][k] * B[k][j];
+//         }
+//         C[i * ldc + j] += c_sub_acc1 + c_sub_acc2;
+//     }
+// }
+// 2）因要划分不同线程处理不同的k的部分，所以要从i和j两层大循环做拆分，
+// 因只split为2段，线程数多一倍，这里选取x方向多一倍线程，则需拆分j循环。
+// for (i = 0; i < M; ++i) {
+//     for (j = 0; j < N; ++j) {
+//         c_sub_acc1 = 0;
+//         for (k = 0; k < K/2; k++) {
+//             c_sub_acc1 += A[i][k] * B[k][j];
+//         }
+//         C[i * ldc + j] += c_sub_acc1;
+//     }
+// }
+// for (i = 0; i < M; ++i) {
+//     for (j = N; j < 2*N; ++j) {
+//         nj = j-N;
+//         c_sub_acc2 = 0;
+//         for (k = K/2; k < K; k++) {
+//             c_sub_acc2 += A[i][k] * B[k][nj];
+//         }
+//         C[i * ldc + nj] += c_sub_acc2;
+//     }
+// }
 template <int BLOCK_SIZE>
 __global__ void GemmKernelv3(const int M, const int N, const int K,
+                             const float *A, const int lda,
+                             const float *B, const int ldb,
+                             float *C, const int ldc) {
+
+    int gid_y = blockIdx.y * blockDim.y + threadIdx.y;
+    int gid_x = blockIdx.x * blockDim.x + threadIdx.x;
+
+    __shared__ float a_shared[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float b_shared[BLOCK_SIZE][BLOCK_SIZE];
+
+    float c_sub_acc_1 = 0;
+    float c_sub_acc_2 = 0;
+    // 按 K 方向分块读入共享内存，一次读一个block
+    if (gid_x < N) {
+        for (int bk = 0; bk < K/2; bk += BLOCK_SIZE) {
+            a_shared[threadIdx.y][threadIdx.x] = A[gid_y * lda + (bk + threadIdx.x)];
+            b_shared[threadIdx.y][threadIdx.x] = B[(bk + threadIdx.y) * ldb + gid_x];
+            // 等待块内线程同步
+            __syncthreads();
+
+            // 计算块内元素
+            for (int k = 0; k < BLOCK_SIZE; k++) {
+                c_sub_acc_1 += a_shared[threadIdx.y][k] * b_shared[k][threadIdx.x];
+            }
+            // 再次同步，避免该块内个别线程已经计算完进入下一次循环中，往共享内存写数据，与正在共享内存正在计算中的数据相冲突
+            __syncthreads();
+        }
+        atomicAdd(&C[gid_y * ldc + gid_x], c_sub_acc_1);
+    }
+    else {
+        for (int bk = K/2; bk < K; bk += BLOCK_SIZE) {
+            a_shared[threadIdx.y][threadIdx.x] = A[gid_y * lda + (bk + threadIdx.x)];
+            b_shared[threadIdx.y][threadIdx.x] = B[(bk + threadIdx.y) * ldb + gid_x-N];
+            // 等待块内线程同步
+            __syncthreads();
+
+            // 计算块内元素
+            for (int k = 0; k < BLOCK_SIZE; k++) {
+                c_sub_acc_2 += a_shared[threadIdx.y][k] * b_shared[k][threadIdx.x];
+            }
+            // 再次同步，避免该块内个别线程已经计算完进入下一次循环中，往共享内存写数据，与正在共享内存正在计算中的数据相冲突
+            __syncthreads();
+        }
+        atomicAdd(&C[gid_y * ldc + gid_x-N], c_sub_acc_2);
+    }
+}
+
+// CUDA version 4.
+//   分析v2，计算的过程实质为全局内存->共享内存->寄存器内存，
+// 而v2的最内层k循环中需重复访问的数据存在于共享内存中，就会有重复的从共享内存读取数据到寄存器的操作。
+// v3的优化思路是使重复读取数据进行计算的操作放到更快的寄存器中完成。、
+// 
+// 如无法理解这个重复读取的过程，可以考虑把M和N的循环放回，转为串行代码，观察a和b的读取次数：
+// for (i = 0; i < M; ++i) {
+//     for (j = 0; j < N; ++j) {
+//         for (k = 0; k < BLOCK_SIZE; k++) {
+//             c_sub_acc += a_shared[i][k] * b_shared[k][j];
+//         }
+//     }
+// }
+// 可以看到对于a_shared[i][k]来说，每个元素重复读取了j次，b_shared[k][j]则对应重复了i次。
+// 则v3的思路是将这个重复从共享内存读取数据做计算的操作，转变为从共享内存中读取一次，在寄存器重复读取数据计算。
+// 可考虑子在一次读取到共享内存后，再进行分块一次读取到寄存器中，则对内存k循环做一次分块操作。提高与共享内存的计算访存比
+// for (i = 0; i < M; i+=STEP) {
+//     for (j = 0; j < N; j+=STEP) {
+//         for (k = 0; k < BLOCK_SIZE; k++) {
+//             for (si = 0; si < STEP; si++) {
+//                 for (sj = 0; sj < STEP; sj++) {
+//                     // c要对应i和j进行累加，i和j放到内层，则需要用STEP*STEP个变量去做累加
+//                     c_sub_acc[i+si][j+sj] += a_shared[i+si][k] * b_shared[k][j+sj];
+//                 }
+//             }
+//         }
+//     }
+// }
+// 看最里面两层循环，a和b涉及的元素各有STEP个，计算次数则是STEP*STEP，所以可以先读到寄存器中，再计算
+// for (i = 0; i < M; i+=STEP) {
+//     for (j = 0; j < N; j+=STEP) {
+//         for (k = 0; k < BLOCK_SIZE; k++) {
+//             for (s = 0; s < STEP; s++) {
+//                 a_reg[s] = a_shared[i+s][k];
+//                 b_reg[s] = b_shared[k][j+s];
+//             }
+//             for (si = 0; si < STEP; si++) {
+//                 for (sj = 0; sj < STEP; sj++) {
+//                     // c要对应i和j进行累加，i和j放到内层，则需要用STEP*STEP个变量去做累加
+//                     c_sub_acc[i+si][j+sj] += a_reg[i+si] * b_reg[k][j+sj];
+//                 }
+//             }
+//         }
+//     }
+// }
+// 观察最外两层i和j循环，在cuda改写时，i和j会被线程的x,y调度隐藏掉。而i和j需要按STEP递增，
+// 同步到cuda则意味着线程(x,y)要处理STEP*STEP个元素，而非原来的1个元素。
+// 则线程布局时，可以是block线程数不变，block数量在x和y方向上减少STEP倍；或减少block内线程数，维持block数量不变。
+template <int BLOCK_SIZE, int STEP>
+__global__ void GemmKernelv4(const int M, const int N, const int K,
                              const float *A, const int lda,
                              const float *B, const int ldb,
                              float *C, const int ldc) {
@@ -235,7 +376,6 @@ __global__ void GemmKernelv3(const int M, const int N, const int K,
     int gid_x = blockIdx.x * blockDim.x + threadIdx.x;
     int gid_y = blockIdx.y * blockDim.y + threadIdx.y;
 
-    const int STEP = 2;
     float a_reg[STEP] = {0};
     float b_reg[STEP] = {0};
     float c_reg[STEP][STEP] = {{0}};
@@ -259,7 +399,7 @@ __global__ void GemmKernelv3(const int M, const int N, const int K,
         // 等待块内线程同步
         __syncthreads();
 
-        // 计算块内元素, 每个线程处理临近四个元素
+        // 计算块内元素, 每个线程处理临近四个元素（如STEP==2）
         // for (int k = 0; k < BLOCK_SIZE*STEP; k++) {
         //     for (int si=0; si<STEP; si++) {
         //         for (int sj=0; sj<STEP; sj++) {
@@ -268,6 +408,7 @@ __global__ void GemmKernelv3(const int M, const int N, const int K,
         //     }
         // }
         for (int k = 0; k < BLOCK_SIZE*STEP; k++) {
+            // 对于a，读取共享内存到寄存器 STEP 次，计算时读取寄存器 STEP*STEP 次; 对于b亦然。
             for (int s = 0; s < STEP; s++) {
                 a_reg[s] = a_shared[tid_sy+s][k];
                 b_reg[s] = b_shared[k][tid_sx+s];
@@ -320,16 +461,17 @@ float MatrixMulCUDA(int version_id, const int M, const int N, const int K,
             (M, N, K, A, lda, B, ldb, C, ldc);    
     }
     else if (version_id == 3) {
-        // 一个线程处理四个数据，一个block内线程数xy都减半，然后block的数量不变。
+        int partition_k = 2;
+        dim3 blocks_per_grid_k(blocks_per_grid.x * partition_k, blocks_per_grid.y); // x方向多开一倍线程
+        GemmKernelv3<block_side_size> << <blocks_per_grid_k, threads_per_block >> >
+            (M, N, K, A, lda, B, ldb, C, ldc);   
+    }
+    else if (version_id == 4) {
+        // 一个线程处理四个数据，block数量xy方向都减半，然后一个block的线程数量不变。
         const int step = 2;
-        // const int block_side_size_new = block_side_size / step;
-        // dim3 threads_per_block_r(block_side_size_new, block_side_size_new);
-        // GemmKernelv3<block_side_size_new> << <blocks_per_grid, threads_per_block_r >> >
-        //     (M, N, K, A, lda, B, ldb, C, ldc);
-
         dim3 blocks_per_grid_r(blocks_per_grid.x/step, blocks_per_grid.y/step);
-        GemmKernelv3<block_side_size> << <blocks_per_grid_r, threads_per_block >> >
-            (M, N, K, A, lda, B, ldb, C, ldc);    
+        GemmKernelv4<block_side_size, step> << <blocks_per_grid_r, threads_per_block >> >
+            (M, N, K, A, lda, B, ldb, C, ldc);
     }
 
     // Record the stop event
@@ -354,8 +496,15 @@ int main() {
         return -1;
     }
 
-    int height_a = 1280, width_a = 4096;
-    int height_b = 4096, width_b = 2048;
+    // // Normal test
+    // int height_a = 1280, width_a = 4096;
+    // int height_b = 4096, width_b = 2048;
+    // // Test split-k
+    int height_a = 64, width_a = 4096;
+    int height_b = 4096, width_b = 64;
+    // // Debug
+    // int height_a = 32, width_a = 64;
+    // int height_b = 64, width_b = 32;
     if (width_a != height_b) {
         printf("width_a should be equal to height_b.\n");
         return 1;
@@ -374,20 +523,20 @@ int main() {
     }
 
     // Initialize 
-    srand(0);
+    srand(time(NULL));
     GenMatrix(height_a, width_a, h_a);
     GenMatrix(height_b, width_b, h_b);
 
-    // CPU
-    time_t t = clock();
-    GemmHostV1(height_a, width_b, width_a, h_a, width_a,h_b, width_b, h_c, width_b);
-    printf("cpu version 1 -> time: %f s, mean value = %f\n", double(clock() - t)/CLOCKS_PER_SEC, GetMean(h_c, height_a, width_b));
-    //MatrixPrint(h_c, height_a, width_b);
+    // // CPU
+    // time_t t = clock();
+    // GemmHostV1(height_a, width_b, width_a, h_a, width_a,h_b, width_b, h_c, width_b);
+    // printf("cpu version 1 -> time: %f s, mean value = %f\n", double(clock() - t)/CLOCKS_PER_SEC, GetMean(h_c, height_a, width_b));
+    // //MatrixPrint(h_c, height_a, width_b);
 
-    t = clock();
-    GemmHostV2(height_a, width_b, width_a, h_a, width_a, h_b, width_b, h_c, width_b);
-    printf("cpu version 2 -> time: %f s, mean value = %f\n", double(clock() - t)/CLOCKS_PER_SEC, GetMean(h_c, height_a, width_b));
-    //MatrixPrint(h_c, height_a, width_b);
+    // t = clock();
+    // GemmHostV2(height_a, width_b, width_a, h_a, width_a, h_b, width_b, h_c, width_b);
+    // printf("cpu version 2 -> time: %f s, mean value = %f\n", double(clock() - t)/CLOCKS_PER_SEC, GetMean(h_c, height_a, width_b));
+    // //MatrixPrint(h_c, height_a, width_b);
 
     // GPU
     // Allocate memory in host. 
@@ -400,14 +549,27 @@ int main() {
     TEST_CUDA_MODULE_UKERNEL(1);
     TEST_CUDA_MODULE_UKERNEL(2);
     TEST_CUDA_MODULE_UKERNEL(3);
+    TEST_CUDA_MODULE_UKERNEL(4);
+    // printf("Print output C:\n");
+    // for (int i=0; i<height_a; i++) {
+    //     for (int j=0; j<width_b; j++) {
+    //         printf("%f, ", h_c[i*width_b+j]);
+    //     }
+    //     printf("\n");
+    // }
 
+    // Normal test.
     // GPU Device 0: "Tesla T4" with compute capability 7.5 with 40 multi-processors.
+    // gpu version 1 -> time: 0.033448 s, mean value = 1178.585693
+    // gpu version 2 -> time: 0.026014 s, mean value = 1178.585693
+    // gpu version 3 -> time: 0.026905 s, mean value = 1178.585693
+    // gpu version 4 -> time: 0.012337 s, mean value = 1178.585693
 
-    // cpu version 1 -> time: 352.808640 s, mean value = 4721666173127589101568.000000
-    // cpu version 2 -> time: 252.558702 s, mean value = 4721666173127589101568.000000
-    // gpu version 1 -> time: 0.035052 s, mean value = 4721666173127589101568.000000
-    // gpu version 2 -> time: 0.027406 s, mean value = 4721666173127589101568.000000
-    // gpu version 3 -> time: 0.013027 s, mean value = 4721666173127589101568.000000
+    // Test split-k
+    // gpu version 1 -> time: 0.000979 s, mean value = 2628.805664
+    // gpu version 2 -> time: 0.000694 s, mean value = 2628.805664
+    // gpu version 3 -> time: 0.000352 s, mean value = 2628.805664 // split-2
+    // gpu version 4 -> time: 0.000965 s, mean value = 2628.805664
     
     free(h_a);
     free(h_b);
